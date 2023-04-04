45e6bf9129cf01193d298d425ce369aba34b115a
Isabelle Bersano <ibersano@microsoft.com>
2023-04-04 16:25:50 -0400

    feat: added vms module

---
+++ scenarios/aca-internal/terraform/modules/vms/main.tf
@@ -0,0 +1,98 @@
+module "nsg" {
+  source        = "../networking/nsg"
+  nsgName       = var.nsgName
+  location      = var.location
+  tags          = var.tags
+  securityRules = var.securityRules
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.vnetName
+  resource_group_name = var.vnetResourceGroupName
+}
+
+resource "azurerm_subnet" "vmSubnet" {
+  name                 = var.vmSubnetName
+  resource_group_name  = data.azurerm_virtual_network.vnet.resource_group_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  address_prefixes     = var.addressPrefixes
+}
+
+resource "azurerm_subnet_network_security_group_association" "bastion" {
+  depends_on = [
+    module.nsg,
+    azurerm_subnet.vmSubnet
+  ]
+  subnet_id                 = azurerm_subnet.vmSubnet.id
+  network_security_group_id = module.nsg.nsgId
+}
+
+resource "azurerm_network_interface" "vmNic" {
+  name                = var.nicName
+  resource_group_name = data.azurerm_virtual_network.vnet.resource_group_name
+  location            = data.azurerm_virtual_network.vnet.location
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.vmSubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "linuxVm" {
+  count = var.osType == "Linux" ? 1 : 0
+
+  name                = var.vmName
+  resource_group_name = var.resourceGroupName
+  location            = var.location
+
+  admin_username = var.adminUsername
+  admin_password = var.adminPassword
+  size           = var.size
+
+  network_interface_ids = [
+    azurerm_network_interface.vmNic
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+}
+
+resource "azurerm_windows_virtual_machine" "windowsVm" {
+  count = var.osType == "Windows" ? 1 : 0
+
+  name                = var.vmName
+  resource_group_name = var.resourceGroupName
+  location            = var.location
+
+  admin_username = var.adminUsername
+  admin_password = var.adminPassword
+  size           = var.size
+
+  network_interface_ids = [
+    azurerm_network_interface.vmNic
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+}
---
+++ scenarios/aca-internal/terraform/modules/vms/variables.tf
@@ -0,0 +1,48 @@
+variable "osType" {
+  default = "Linux"
+}
+
+variable "nsgName" {
+  default = ""
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "tags" {
+}
+
+variable "vnetName" {
+}
+
+variable "vnetResourceGroupName" {
+}
+
+variable "addressPrefixes" {
+}
+
+variable "securityRules" {
+}
+
+variable "nicName" {
+}
+
+variable "vmName" {
+}
+
+variable "adminUsername" {
+}
+
+variable "adminPassword" {
+}
+
+variable "resourceGroupName" {
+}
+
+variable "size" {
+}
+
+variable "vmSubnetName" {
+    
+}

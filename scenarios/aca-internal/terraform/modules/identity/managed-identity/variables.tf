a988a75c561bdc9e5a88fea74569d76ab866dcec
Isabelle Bersano <ibersano@microsoft.com>
2023-04-04 15:08:18 -0400

    feat: added managed identity module

---
+++ scenarios/aca-internal/terraform/modules/identity/managed-identity/variables.tf
@@ -0,0 +1,21 @@
+variable "managedIdentityName" {
+  default = ""
+  type = string
+  validation {
+      condition = length(var.managedIdentityName) < 3 || length(var.managedIdentityName) > 128
+      error_message = "Name must be greater than 3 characters and not longer than 128 characters."
+    }
+}
+
+variable "resourceGroupName" {
+  type = string
+}
+
+variable "location" {
+    default = "eastus"
+    type = string
+}
+
+variable "tags" {
+  
+}

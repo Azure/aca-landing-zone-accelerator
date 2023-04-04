9d2c64e8d768d005d44839ba5aa2c9762ec6a92f
Isabelle Bersano <ibersano@microsoft.com>
2023-04-04 15:00:36 -0400

    feat: added app insights module

---
+++ scenarios/aca-internal/terraform/modules/monitoring/app-insights/variables.tf
@@ -0,0 +1,66 @@
+variable "appInsightsName" {
+  default = ""
+  type = string
+  validation {
+      condition = length(var.appInsightsName) < 4 || length(var.appInsightsName) > 63
+      error_message = "Name must be greater than 4 characters and not longer than 63 characters."
+    }
+}
+
+variable "resourceGroupName" {
+  type = string
+}
+
+variable "location" {
+    default = "eastus"
+    type = string
+}
+
+variable "retentionInDays" {
+  default = 90
+  type = number
+  validation {
+    condition = var.retentionInDays <= 730
+    error_message = "Value can't be more than 730 days."
+  }
+}
+
+variable "tags" {
+    
+}
+
+variable "ingestionEnabled" {
+  default = true
+  type = bool
+}
+
+variable "internetQueryEnabled" {
+    default = true
+    type = bool
+  
+}
+
+variable "samplingPercentage" {
+    default = 100
+    type = number
+    validation {
+      condition = var.samplingPercentage < 0 || var.samplingPercentage > 100
+      error_message = "Value must be between 0 and 100."
+    }
+}
+
+variable "applicationType" {
+    default = "web"
+    type = string
+    validation {
+    condition = anytrue([
+      var.sku == "web",
+      var.sku == "other"
+    ])
+    error_message = "Application Type must be either web or other."
+    }
+}
+
+variable "workspaceId" {
+    type = string
+}

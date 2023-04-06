1624fb1e6f9a7c480021a0c1a40c703af7ef2344
Isabelle Bersano <ibersano@microsoft.com>
2023-04-06 14:23:28 -0400

    fix: updated security rules for bastion nsg

--- scenarios/shared/terraform/modules/bastion/variables.tf
+++ scenarios/shared/terraform/modules/bastion/variables.tf
@@ -11,6 +11,94 @@ variable "bastionNsgName" {
 }
 
 variable "securityRules" {
+  default = [{
+    "name"                     = "AllowHttpsInbound"
+    "priority"                 = 120
+    "protocol"                 = "Tcp"
+    "destinationPortRanges"     = ["443"]
+    "access"                   = "Allow"
+    "direction"                = "Inbound"
+    "sourcePortRange"          = "*"
+    "sourceAddressPrefix"      = "Internet"
+    "destinationAddressPrefix" = "*"
+    },
+    {
+      "name"                     = "AllowGatewayManagerInbound"
+      "priority"                 = 130
+      "protocol"                 = "Tcp"
+      "destinationPortRanges"     = ["443"]
+      "access"                   = "Allow"
+      "direction"                = "Inbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "GatewayManager"
+      "destinationAddressPrefix" = "*"
+    },
+    {
+      "name"                     = "AllowAzureLoadBalancerInbound"
+      "priority"                 = 140
+      "protocol"                 = "Tcp"
+      "destinationPortRanges"     = ["443"]
+      "access"                   = "Allow"
+      "direction"                = "Inbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "AzureLoadBalancer"
+      "destinationAddressPrefix" = "*"
+    },
+    {
+      "name"                     = "AllowBastionHostCommunicationInbound"
+      "priority"                 = 150
+      "protocol"                 = "*"
+      "destinationPortRanges"    = ["8080", "5701"]
+      "access"                   = "Allow"
+      "direction"                = "Inbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "VirtualNetwork"
+      "destinationAddressPrefix" = "VirtualNetwork"
+    },
+    {
+      "name"                     = "AllowSshRdpOutbound"
+      "priority"                 = 100
+      "protocol"                 = "*"
+      "destinationPortRanges"    = ["22", "3389"]
+      "access"                   = "Allow"
+      "direction"                = "Outbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "*"
+      "destinationAddressPrefix" = "VirtualNetwork"
+    },
+    {
+      "name"                     = "AllowAzureCloudOutbound"
+      "priority"                 = 110
+      "protocol"                 = "Tcp"
+      "destinationPortRanges"     = ["443"]
+      "access"                   = "Allow"
+      "direction"                = "Outbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "*"
+      "destinationAddressPrefix" = "AzureCloud"
+    },
+    {
+      "name"                     = "AllowBastionCommunication"
+      "priority"                 = 120
+      "protocol"                 = "*"
+      "destinationPortRanges"    = ["8080", "5701"]
+      "access"                   = "Allow"
+      "direction"                = "Outbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "VirtualNetwork"
+      "destinationAddressPrefix" = "VirtualNetwork"
+    },
+    {
+      "name"                     = "AllowGetSessionInformation"
+      "priority"                 = 130
+      "protocol"                 = "*"
+      "destinationPortRanges"     = ["80"]
+      "access"                   = "Allow"
+      "direction"                = "Outbound"
+      "sourcePortRange"          = "*"
+      "sourceAddressPrefix"      = "*"
+      "destinationAddressPrefix" = "Internet"
+  }]
 
 }
 
@@ -31,5 +119,5 @@ variable "bastionHostName" {
 }
 
 variable "location" {
-  
+
 }

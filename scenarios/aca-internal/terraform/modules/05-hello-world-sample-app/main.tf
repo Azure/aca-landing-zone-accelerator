41ba9ee8007ab309499f3d4dc6bc5cc9e1ca831f
Isabelle Bersano <ibersano@microsoft.com>
2023-04-19 14:37:52 -0400

    feat: added hello world sample app module

---
+++ scenarios/aca-internal/terraform/modules/05-hello-world-sample-app/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_container_app" "helloWorld" {
+  name                         = var.helloWorldContainerAppName
+  resource_group_name          = var.resourceGroupName
+  container_app_environment_id = var.containerAppsEnvironmentId
+  tags                         = var.tags
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [var.containerRegistryUserAssignedIdentityId]
+  }
+
+  revision_mode = "Single"
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 80
+    transport                  = "auto"
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  template {
+    container {
+      name   = "simple-hello"
+      cpu    = "0.25"
+      memory = "0.5Gi"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+    }
+
+    min_replicas = 1
+    max_replicas = 10
+  }
+}

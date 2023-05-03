0964ddb8536c6cd88f776aed0f3d0b354655c261
Houssem Dellai <hodellai@microsoft.com>
2023-05-03 16:16:14 +0200

    editing terraform config

--- scenarios/shared/scripts/jumpbox-setup-cli-tools.sh
+++ scenarios/shared/scripts/jumpbox-setup-cli-tools.sh
@@ -25,31 +25,13 @@ wget -qO- https://get.docker.com/ | sh >> $logpath
 sudo usermod -aG docker $1
 echo " " >> $logpath
 
-# Install Microsoft OpenJDK 17
-ubuntu_release=`lsb_release -rs`
-wget https://packages.microsoft.com/config/ubuntu/${ubuntu_release}/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install msopenjdk-17 -y
-
-# Install Maven
-wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz -P /tmp
-sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-3.8.7 /opt/maven
-
-echo "export JAVA_HOME=/usr/lib/jvm/msopenjdk-17-amd64" >> ~/.bashrc
-echo "export M2_HOME=/opt/maven" >> ~/.bashrc
-echo "export MAVEN_HOME=/opt/maven" >> ~/.bashrc
-echo "export PATH=${M2_HOME}/bin:${PATH}" >> ~/.bashrc
-exit
-
-source ~/.bashrc
-
 
 # install Terraform CLI
 # https://learn.hashicorp.com/tutorials/terraform/install-cli
+echo "#############################" >> $logpath
+echo "Installing Terraform" >> $logpath
+echo "#############################" >> $logpath
+
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 
 wget -O- https://apt.releases.hashicorp.com/gpg | \

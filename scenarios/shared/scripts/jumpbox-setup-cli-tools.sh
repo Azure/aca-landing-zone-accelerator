4cee1d07458b4846da7d1b6c639b6ee6e01912b8
Houssem Dellai <hodellai@microsoft.com>
2023-05-03 16:32:14 +0200

    editing terraform config

--- scenarios/shared/scripts/jumpbox-setup-cli-tools.sh
+++ scenarios/shared/scripts/jumpbox-setup-cli-tools.sh
@@ -32,22 +32,8 @@ echo "#############################" >> $logpath
 echo "Installing Terraform" >> $logpath
 echo "#############################" >> $logpath
 
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-gpg --dearmor | \
-sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-gpg --no-default-keyring \
---keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
---fingerprint
-
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-sudo apt update
-
-sudo apt-get install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 
 terraform version
